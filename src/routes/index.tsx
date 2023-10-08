8d678403e33715e97ae2fa622db8635f578198a8
JGuilhermeCoelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2023-10-08 19:27:12 -0300

    add screen to change profile informations

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack';
-import HomeScreen from '../screens/home/HomeScreen';
-import Login from "../screens/login/Login";
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from '../screens/home/HomeScreen'
+import Login from '../screens/login/Login'
 
-import {defaultScreenOptions} from './types';
-import SignUp from "../screens/create-account/SignUp";
-import GroupScreen from "../screens/grupo/GroupScreen";
-import History from "../screens/history/History";
+import { defaultScreenOptions } from './types'
+import SignUp from '../screens/create-account/SignUp'
+import GroupScreen from '../screens/grupo/GroupScreen'
+import History from '../screens/history/History'
+import ChangeFirstNameScreen from '../screens/change-name/ChangeFirstNameScreen'
+import ChangeLastNameScreen from '../screens/change-name/ChangeLastNameScreen'
+import Profile from '../screens/profile/ProfileScreen'
+import ChangeEmail from '../screens/change-email/ChangeEmail'
+import ChangePassword from '../screens/change-password/ChangePassword'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName={"Login"} screenOptions={defaultScreenOptions}>
-                <Stack.Screen name="Login" component={Login}/>
-                <Stack.Screen name="Home" component={HomeScreen}/>
-                <Stack.Screen name="GroupScreen" component={GroupScreen}/>
-                <Stack.Screen name="SignUp" component={SignUp}/>
-                <Stack.Screen name="History" component={History}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={'ChangePassword'}
+        screenOptions={defaultScreenOptions}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="GroupScreen" component={GroupScreen} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="History" component={History} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen
+          name="ChangeFirstNameScreen"
+          component={ChangeFirstNameScreen}
+        />
+        <Stack.Screen
+          name="ChangeLastNameScreen"
+          component={ChangeLastNameScreen}
+        />
+        <Stack.Screen name="ChangeEmail" component={ChangeEmail} />
+        <Stack.Screen name="ChangePassword" component={ChangePassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
---
+++ src/screens/change-email/ChangeEmail.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react'
+import { AntDesign } from '@expo/vector-icons'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'
+
+const ChangeEmail = () => {
+  return (
+    <View style={styles.container}>
+      <AntDesign
+        name="arrowleft"
+        size={30}
+        color="white"
+        style={styles.arrow}
+      />
+
+      <Text style={[styles.title]}>Alterar e-mail</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput value={`meuemail@gmail.com`} style={styles.input} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Atualizar e-mail</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#373B3F',
+    height: '100%',
+  },
+  arrow: {
+    position: 'absolute',
+    left: 30,
+    top: 50,
+  },
+  title: {
+    top: 90,
+    left: '8.72%',
+    fontSize: 24,
+    lineHeight: 32,
+    textAlign: 'left',
+    position: 'absolute',
+    color: '#fff',
+    fontWeight: '700',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '90%',
+    padding: 10,
+    borderRadius: 8,
+    borderColor: '#1CC29F',
+    color: 'white',
+    paddingVertical: 12,
+    top: 152,
+    right: '6.15%',
+    left: '6.15%',
+    position: 'absolute',
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+  },
+  input: {
+    width: '95%',
+    backgroundColor: '#373B3F',
+    borderColor: 'transparent',
+    color: 'white',
+  },
+  buttonContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    height: 60,
+  },
+  button: {
+    top: 260,
+
+    width: '90%',
+    height: 55,
+    backgroundColor: '#1CC29F',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+  },
+})
+
+export default ChangeEmail
